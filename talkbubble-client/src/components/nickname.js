2289779509098f09adbb836d18d45a3726583553
Colin Maresh <cmaresh57@gmail.com>
2021-01-12 11:31:53 -0600

    removed inline svg tags

--- talkbubble-client/src/components/nickname.js
+++ talkbubble-client/src/components/nickname.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import {ReactComponent as Confirm_SVG} from '../media/confirm.svg';
+import {ReactComponent as Cancel_SVG} from '../media/cancel.svg';
 
 const NicknameElem = styled.div`
     display: flex;
@@ -84,10 +86,10 @@ export function Nickname(props) {
     return (
         <NicknameElem>
             <NicknameOpt className={"nickname-option accept " + (updatingNickname ? 'active' : '')} onClick={nicknameAccept}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/></svg>
+                <Confirm_SVG />
             </NicknameOpt>
             <NicknameOpt className={"nickname-option reject " + (updatingNickname ? 'active' : '')} onClick={() => { setUpdatingNickname(false); }}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>
+                <Cancel_SVG />
             </NicknameOpt>
             <PostingAs>Posting as: 
             <NicknameText type="text" value={getNicknameText()} onChange={nicknameChange} onClick={nicknameUpdate} />
